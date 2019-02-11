6ce0d776b5ed7a62532a565a2339ac7a2323c730
porao <ratomorto@riseup.net>
2019-02-10 22:17:03 -0200

    script dividido em funcoes

--- js/total.js
+++ js/total.js
@@ -1,15 +1,27 @@
-var input_quantidade = document.querySelector("#qtd");
-var output_total = document.querySelector("#total");
-    
+var inputQuantidade = document.querySelector("#qtd");
+var outputTotal = document.querySelector("#total");
 
-input_quantidade.oninput = function() {
+inputQuantidade.oninput = function () {
     var preco = document.querySelector("#preco").textContent;
-    preco = preco.replace("R$ ", "");
-    preco = preco.replace(",", ".");
-    preco = parseFloat(preco);
-    var quantidade = input_quantidade.value;
+    var preco = textToFloat(preco);
+    
+    var quantidade = inputQuantidade.value;
+    
     var total = quantidade * preco;
-    total = "R$ " + total.toFixed(2)
-    total = total.replace(".", ",");
-    output_total.value = total;
+    
+    outputTotal.value = floatToText(total);   
+}
+
+//transforma texto html em numeros
+function textToFloat(text){
+    text = text.replace("R$ ", "").replace(",", ".");
+    return(parseFloat(text));
+}
+
+//transforma os numeros em texto html "R$ + NUMERO "
+function floatToText (number) {
+    number = "R$ " + number.toFixed(2); // adiciona R$ e para nas 2 casas decimais
+    number = number.replace(".", ","); //troca ponto pela virgula(sistema br)
+    
+    return number;
 }
