b652a3d4cbe97730c031a267080788c9d01aeb06
porao <ratomorto@riseup.net>
2019-02-10 23:07:14 -0200

    funcao para/continua a troca de imagens adicionada - ao clicar na imagm

--- js/banner.js
+++ js/banner.js
@@ -1,8 +1,23 @@
 var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
 var bannerAtual = 0;
-
+var imagemBanner = document.querySelector('#bannerDestaque');
 function trocaBanner(){
     bannerAtual = (bannerAtual+1) % 2;
-    document.querySelector('#bannerDestaque').src = banners[bannerAtual]; 
+    imagemBanner.src = banners[bannerAtual]; 
     }
-setInterval(trocaBanner, 4000);
+var timer = setInterval(trocaBanner, 4000);
+var x = 0;
+
+imagemBanner.onclick = function () {
+    if(x==0) { 
+        clearInterval(timer);
+        
+        alert('hm...');
+        x=1
+        }
+    else{
+        timer = setInterval(trocaBanner, 4000);
+        alert('dois...');
+        x=0;
+    }
+}
